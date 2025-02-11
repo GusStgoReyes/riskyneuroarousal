4f5af44e24e5562f94613f07881cdb01b7fa939d
gusxtsr <gustxsr@mit.edu>
2025-02-10 18:10:30 -0800

    HDDM model changes

--- src/modeling/hddm_model.py
+++ src/modeling/hddm_model.py
@@ -1,7 +1,8 @@
 import hddm
-
-def load_data(subj_ID):
-    data = hddm.load_csv("/scratch/users/gustxsr/PoldrackLab/riskyneuroarousal/data/behavioral_data.csv").query(f"sub == {subj_ID}")
+# TODO: Make paths come from config file (but also, caring for the installation of package...)
+# HDDM runs in a container. 
+def load_data(subj_ID, data_path = "/scratch/users/gustxsr/PoldrackLab/riskyneuroarousal/data/behavioral_data.csv"):
+    data = hddm.load_csv(data_path).query(f"sub == {subj_ID}")
     data = data.rename(columns={"RT" : "rt", "accept" : "response", "sub" : "subj_idx"})
     data = data.query("rt > 0.2")
     data = hddm.utils.flip_errors(data)
@@ -11,13 +12,14 @@ if __name__ == "__main__":
     import argparse
     parser = argparse.ArgumentParser(description="Fit a DDM model to a subject's data")
     parser.add_argument("--subj_ID", type=int, help="The subject ID to fit the model to")
+    parser.add_argument("--data_pth", type=str, help="The path to the data")
     parser.add_argument("--model", type=str, help="The model to fit the data to (from 1 to 3)")
     parser.add_argument("--results_pth", type=str, help="The path to save the results")
     parser.add_argument("--model_pth", type=str, help="The path to save the model")
     args = parser.parse_args()
 
     # Load a subjects data (TODO: make dynamic src path)
-    data = load_data(int(args.subj_ID))
+    data = load_data(int(args.subj_ID), args.data_pth)
 
     if args.model == "1":
         # Model with alpha, drift_gain, drift_loss, boundary, response_bias, non_decision time, and collapsing boundary
