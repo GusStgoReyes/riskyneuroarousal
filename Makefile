1402dacde9e415d73dd28430744769b83a121e0c
Russell Poldrack <poldrack@gmail.com>
2025-02-13 11:53:20 -0800

    format src

--- Makefile
+++ Makefile
@@ -1,6 +1,9 @@
 test:
 	python -m pytest tests
 
+format-src:
+	ruff format src/riskyneuroarousal/*.py
+	ruff format src/riskyneuroarousal/*/*.py
 
 format-notebooks:
 	ruff format notebooks/*
--- src/riskyneuroarousal/utils/load_data.py
+++ src/riskyneuroarousal/utils/load_data.py
@@ -2,30 +2,33 @@ import pandas as pd
 import os
 from riskyneuroarousal.config import Config
 
+
 def load_config():
     """
-        Load the configuration file.
+    Load the configuration file.
     """
     # Get path to the data from the config file
     config = Config()
     username = config.username
     return config, username
 
-def load_behavioral_data(min_RT = 0.2):
+
+def load_behavioral_data(min_RT=0.2):
     """
-        Load behavioral data from the specified file path.
+    Load behavioral data from the specified file path.
     """
     config, username = load_config()
     data_path = os.path.join(config.data_path[f"{username}"], "behavioral_data.csv")
 
     # Load the data and remove trials with RT < 0.2 (includes no response trials)
-    data = pd.read_csv(data_path).query(f'RT > {min_RT}')
+    data = pd.read_csv(data_path).query(f"RT > {min_RT}")
 
     return data
 
+
 def load_pt_results():
     """
-        Load the Prospect theory parameters modeled
+    Load the Prospect theory parameters modeled
     """
     config, username = load_config()
     data_path = os.path.join(config.data_path[f"{username}"], "pt_results.csv")
@@ -35,9 +38,10 @@ def load_pt_results():
 
     return data
 
+
 def load_ddm_results():
     """
-        Load the DDM parameters modeled
+    Load the DDM parameters modeled
     """
     config, username = load_config()
     data_path = os.path.join(config.data_path[f"{username}"], "ddm_parameters.csv")
@@ -47,9 +51,10 @@ def load_ddm_results():
 
     return data
 
+
 def load_pupil_results():
     """
-        Load the pupil results
+    Load the pupil results
     """
     config, username = load_config()
     data_path = os.path.join(config.data_path[f"{username}"], "pupil_coefs.csv")
@@ -58,6 +63,7 @@ def load_pupil_results():
     data = pd.read_csv(data_path)
 
     return data
+
 
 def load_pupil_data():
     """
@@ -70,6 +76,7 @@ def load_pupil_data():
     data = pd.read_csv(data_path)
 
     return data
+
 
 ## SCRAP!!!
 # # Load all the pupil data
